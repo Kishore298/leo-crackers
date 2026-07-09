17d9ea3f5cd78487024b7a5e11981a19669184c0
Kishore298 <kishore.2002@sritcbe.ac.in>
2026-07-09 15:44:14 +0530

    changes: nodemailer to smtp change,download price list feature

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -8,7 +8,7 @@ const createTransporter = () => {
   
   return nodemailer.createTransport({
     host: 'smtp-relay.brevo.com',
-    port: 587,
+    port: 2525,
     secure: false, // true for 465, false for other ports
     auth: {
       user: process.env.EMAIL_USER,
